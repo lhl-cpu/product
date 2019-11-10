b7e52f81ac7ad398f1d9b1990a4e5fda68254561
lhl-cpu <289041280@qq.com>
2019-11-10 21:21:40 +0800

    增加登录和退出功能

--- ph/account/urls.py
+++ ph/account/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('sign/', views.signin,name='注册页面'),
+    path('login/', views.login,name='登录页面'),
+    path('logout/', views.logout,name='退出页面'),
 ]
--- ph/account/views.py
+++ ph/account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User #注册
+from django.contrib import auth #登录
 # Create your views here.
 
 
@@ -19,3 +20,22 @@ def signin(request):
 				return redirect('主页')
 			else:
 				return render(request,'sign.html',{'密码错误':'两次密码不一致'})
+
+
+def login(request):
+	if request.method == 'GET':
+		return render(request,'login.html')
+	elif request.method == 'POST':
+		user_name = request.POST['用户名']
+		password = request.POST['密码']
+		user = auth.authenticate(username=user_name,password=password)
+		if user is None:
+			return render(request,'login.html',{'信息错误':'请输入正确的用户名和密码！'})
+		else:
+			auth.login(request, user)
+			return redirect('主页')
+
+def logout(request):
+	if request.method == 'POST':
+		auth.logout(request)
+		return redirect('主页')
