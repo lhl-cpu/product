bed77f6b9d45f1c003918e00fd8de3b540f33402
lhl-cpu <289041280@qq.com>
2019-11-12 11:08:14 +0800

    修复不能上传的bug

--- ph/products/views.py
+++ ph/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect, get_object_or_404
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Product
 from django.utils import timezone
